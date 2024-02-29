8deaeba4d0587c3e99831cfa0ecd4bc5c44f3872
Atharva <63628518+atharva-malik@users.noreply.github.com>
2024-02-29 12:54:26 +1100

    added bones for signup

--- run.py
+++ run.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from email_validator import validate_email
+THIS !!!! !!!!! is_valid = validate_email('example@foasdccvo.com', check_deliverability=True)
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+#region
+"""
+api_key = "ede8ff9e889a72caabf0bcec094eb623"
+
+
+city = 'London'
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+# Send GET request and handle response
+response = requests.get(url)
+print(response)
+if response.status_code == 200:
+  data = response.json()
+  temp = data['main']['temp']
+  description = data['weather'][0]['description'].capitalize()
+  print(f"Weather in {city}: {temp:.2f}°C, {description}")
+else:
+  print("Error: Could not retrieve weather data.")
+"""
+#endregion
+
+
+@app.route("/", methods = ['POST', 'GET'])
 def home():
     return render_template("home.html")
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+  if request.method == 'POST':
+    # Your logic to handle form submission (e.g., validate data, save to database)
+    return str("ASdasdf")
+  return render_template('signup.html')
+
 if __name__ == "__main__":
+    print(is_valid.normalized)
     app.run(debug=True)
---
+++ todo.md
@@ -0,0 +1,7 @@
+1. Signin, Guest and signup
+2. City search (maybe optimise using autofil?)
+3. Show user past searches
+4. In the api: hourly and weekly (can switch in the press of a button)
+5. Provide weather alerts (Severe weather warning. IN RED!!!)
+6. Show historical weather data (What it is like over the years)
+7. Sunrise/Sunset Times
